01971ce7bfecdbbf6a1bbe67a8bc7004987d1c0a
spnraju <padmanabharaju.sagi@gmail.com>
2021-10-17 03:06:05 +0530

    update node runtime to 14 for builds

--- tests/phptravels.spec.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'DuckduckGo Launch' : function (browser) {
-    browser
-      .url('https://www.phptravels.net/home')
-      .waitForElementVisible('body')
-      .assert.titleContains('Technology')
-      .end();
-  }
-};
